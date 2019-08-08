e8778a6c42e58c87409abd401cfa10ad7f822bba
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-08-08 18:52:07 +0530

    Added following validations for product model - Given title: not empty, uniq - Given price: non zero, float, non empty - Given description: non empty - Implemented further validation for title and price combination as uniq

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,8 @@ class Product
   field :thumb_image, type: String
 
   embedded_in :user   # Given Parent relationship
+
+  validates :title, :price, :description, presence: true
+  validates :title, uniqueness: true
+  validates :price, uniqueness: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
 end
