a935e6a6bbe0ee1fa025680d268174fe130d6602
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-08-09 11:54:10 +0530

    Integrated About page in application
    - Added a image for about page
    - Generated a route to about page
    - About and Contact links are added to application navbar

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
+	def product_image img, type
+		if img
+			img
+		elsif type =='thumb'
+			image_generator(height: 350 , width: 200)
+		else type == 'main'
+			image_generator(height: 600 , width: 400)
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products
   get 'pages/index'
   get 'pages/contact'
+  get 'pages/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
